276e6f56c3db4327388544248f7c16921a7646e9
Dongyu Zhao <danielzhaowise@gmail.com>
2020-02-14 17:50:30 -0500

    feat: fix models and add scripts to generate fake data in seed

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,4 +1,4 @@
 class Event < ApplicationRecord
     has_many :participants
-    has_many :users, through participants
+    has_many :users, through: :participants
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ApplicationRecord
     mount_uploader :portrait, PortraitUploader
     has_many :participants
-    has_many :events, through participants
+    has_many :events, through: :participants
 end
